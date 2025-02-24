794ef364c57eb299dc26211432f446c060f6027e
Artol Jano <artol.jano0@gmail.com>
2025-02-24 17:31:56 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,55 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.tsx';
-import SuggestGuest from './SuggestGuest.tsx';
-import ReviewPodcast from './ReviewPodcast.tsx';
-import Episodes from './Episodes.tsx';
-import About from './About.tsx';
-import Newsletter from './Newsletter.tsx';
-import Layout from './Layout.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import SuggestGuest from "./SuggestGuest.tsx";
+import ReviewPodcast from "./ReviewPodcast.tsx";
+import Episodes from "./Episodes.tsx";
+import About from "./About.tsx";
+import Newsletter from "./Newsletter.tsx";
+import Layout from "./Layout.tsx";
+import "./index.css";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <App />,
+        },
+        {
+          path: "/suggest",
+          element: <SuggestGuest />,
+        },
+        {
+          path: "/review",
+          element: <ReviewPodcast />,
+        },
+        {
+          path: "/episodes",
+          element: <Episodes />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/newsletter",
+          element: <Newsletter />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <App />,
-      },
-      {
-        path: "/suggest",
-        element: <SuggestGuest />,
-      },
-      {
-        path: "/review",
-        element: <ReviewPodcast />,
-      },
-      {
-        path: "/episodes",
-        element: <Episodes />,
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/newsletter",
-        element: <Newsletter />,
-      },
-    ],
-  },
-]);
+    basename: "/tru", // Replace with your actual GitHub repository name
+  }
+);
 
-createRoot(document.getElementById('root')!).render(
+// Render the app with the router
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
